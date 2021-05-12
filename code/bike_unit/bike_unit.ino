e6cb84455e5e3a056e1e033265dc4604c5a86032
Amit Kharade <amitkharade8200@gmail.com>
2021-05-12 15:01:07 +0400

    Final Code

--- code/bike_unit/bike_unit.ino
+++ code/bike_unit/bike_unit.ino
@@ -33,6 +33,7 @@ void setup() {
 
 void displaylcd(char text);
 void crash(float pitch, float roll);
+
 void loop() {
   if (radio.available()) {
     char text[32] = "";
@@ -43,40 +44,45 @@ void loop() {
     Serial.print("\nrx data\n");
     void displaylcd(char text);
 
-    if(text=="SORRY Engine stop"){ // Stop ignition incase of alcohol detected 
-      digitalWrite(stop_pin,HIGH);
-      Serial.print("Engine stop");
-    }
+      if(text=="SORRY Engine stop"){ // Stop ignition incase of alcohol detected 
+       digitalWrite(stop_pin,HIGH);
+       Serial.print("Engine stop");
+     }
   
-  int x_adc_value, y_adc_value, z_adc_value; 
-  double x_g_value, y_g_value, z_g_value;
-  double roll, pitch;
-  x_adc_value = analogRead(x_out); // Digital value of voltage on x_out pin  
-  y_adc_value = analogRead(y_out); // Digital value of voltage on y_out pin  
-  z_adc_value = analogRead(z_out); // Digital value of voltage on z_out pin  
-  Serial.print("x = ");
-  Serial.print(x_adc_value);
-  Serial.print("\t\t");
-  Serial.print("y = ");
-  Serial.print(y_adc_value);
-  Serial.print("\t\t");
-  Serial.print("z = ");
-  Serial.print(z_adc_value);
-  Serial.print("\t\t");
-  delay(100);
-  x_g_value = ( ( ( (double)(x_adc_value * 5)/1024) - 1.65 ) / 0.330 ); // Acceleration in x-direction in g 
-  y_g_value = ( ( ( (double)(y_adc_value * 5)/1024) - 1.65 ) / 0.330 ); // Acceleration in y-direction in g   
-  z_g_value = ( ( ( (double)(z_adc_value * 5)/1024) - 1.80 ) / 0.330 ); // Acceleration in z-direction in g  
-  roll = ( ( (atan2(y_g_value,z_g_value) * 180) / 3.14 ) + 180 );       // Formula for roll 
-  pitch = ( ( (atan2(z_g_value,x_g_value) * 180) / 3.14 ) + 180 );      // Formula for pitch 
-  Serial.print("Roll = ");
-  Serial.print(roll);
-  Serial.print("\t");
-  Serial.print("Pitch = ");
-  Serial.print(pitch);
-  Serial.print("\n\n");
-  delay(1000);
-  }
+    int x_adc_value, y_adc_value, z_adc_value; 
+   double x_g_value, y_g_value, z_g_value;
+   double roll, pitch;
+   x_adc_value = analogRead(x_out); // Digital value of voltage on x_out pin  
+   y_adc_value = analogRead(y_out); // Digital value of voltage on y_out pin  
+   z_adc_value = analogRead(z_out); // Digital value of voltage on z_out pin  
+   Serial.print("x = ");
+   Serial.print(x_adc_value);
+   Serial.print("\t\t");
+   Serial.print("y = ");
+   Serial.print(y_adc_value);
+   Serial.print("\t\t");
+   Serial.print("z = ");
+   Serial.print(z_adc_value);
+   Serial.print("\t\t");
+   delay(100);
+   x_g_value = ( ( ( (double)(x_adc_value * 5)/1024) - 1.65 ) / 0.330 ); // Acceleration in x-direction in g 
+   y_g_value = ( ( ( (double)(y_adc_value * 5)/1024) - 1.65 ) / 0.330 ); // Acceleration in y-direction in g   
+   z_g_value = ( ( ( (double)(z_adc_value * 5)/1024) - 1.80 ) / 0.330 ); // Acceleration in z-direction in g  
+   roll = ( ( (atan2(y_g_value,z_g_value) * 180) / 3.14 ) + 180 );       // Formula for roll 
+   pitch = ( ( (atan2(z_g_value,x_g_value) * 180) / 3.14 ) + 180 );      // Formula for pitch 
+   Serial.print("Roll = ");
+   Serial.print(roll);
+   Serial.print("\t");
+   Serial.print("Pitch = ");
+   Serial.print(pitch);
+   Serial.print("\n\n");
+   delay(1000);
+   }
+   else{                                    // In case NRF dont get signal
+    Serial.print("Helmet not detected\n"); 
+    const char msg[]=" WEAR HELMET ";       // Wear helmet  
+    void displaylcd(char msg);
+   }
 }
 void displaylcd(char text)  {      // Display receiving data
     lcd.setCursor(0, 0);     
